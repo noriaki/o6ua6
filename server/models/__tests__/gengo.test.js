26b617150be532778ccf49fd8638cb8f8a734f17
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-06-18 23:44:35 +0900

    add data, add import method

--- server/models/__tests__/gengo.test.js
+++ server/models/__tests__/gengo.test.js
@@ -24,4 +24,26 @@ describe('Gengo model', () => {
       expect(subject.identifier).toBe('99696e');
     });
   });
+
+  describe('data import', () => {
+    it('format(json)-> { [surface]: [yomi01, yomi02, ...] }', async () => {
+      /* eslint-disable quote-props */
+      const subjects = {
+        '明治': ['メイジ', 'メイチ'],
+        '大正': ['タイショウ', 'ダイショウ'],
+        '昭和': ['ショウワ'],
+        '平成': ['ヘイセイ'],
+      };
+      /* eslint-enable quote-props */
+      await Gengo.import(subjects);
+      expect(await Gengo.count()).toBe(Object.keys(subjects).length);
+      const expectedYomi = flatten(Object.values(subjects));
+      const subjectYomi = flatten((await Gengo.find()).map(g => g.yomi));
+      expect(subjectYomi).toHaveLength(expectedYomi.length);
+    });
+  });
 });
+
+const flatten = arrayOfArray => (
+  arrayOfArray.reduce((ret, array) => ret.concat(array), [])
+);
--- server/models/gengo.js
+++ server/models/gengo.js
@@ -1,6 +1,9 @@
+const { join } = require('path');
+const { readFileSync } = require('fs');
 const mongoose = require('mongoose');
 const { yomiSchema } = require('./yomi');
 const Rating = require('./rating');
+
 const identifier = require('./concerns/identifier');
 
 const { Schema } = mongoose;
@@ -23,5 +26,23 @@ gengoSchema.pre('validate', function setIdentifier(next) {
 
 const Gengo = mongoose.model('Gengo', gengoSchema);
 Gengo.gengoSchema = gengoSchema;
+
+// @async
+Gengo.import = function importGengo(list = {}) {
+  const gengos = Object.keys(list);
+  const promises = gengos.map((surface) => {
+    const yomis = list[surface];
+    const gengo = new this({ surface });
+    gengo.yomi = yomis.map(name => ({ name }));
+    return gengo.save();
+  });
+  return Promise.all(promises);
+};
+// @async
+Gengo.importFromFile = function importGengoFromFile() {
+  const filepath = join(__dirname, '..', '..', 'db', 'OnyomiGengoMap.json');
+  const list = JSON.parse(readFileSync(filepath, 'utf8'));
+  return this.import(list);
+};
 
 module.exports = Gengo;

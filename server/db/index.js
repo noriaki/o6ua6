57a309b1e74d320fef4d953f1b3a685de5324f8c
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2018-07-02 20:09:16 +0900

    modify conncting db

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose');
 
 const connect = async ({ env } = { env: process.env.NODE_ENV }) => {
-  const map = { development: '_dev', test: '_test', production: '' };
-  const LOCAL_MONGODB_URI = `mongodb://localhost:27017/o6ua6_next${map[env]}`;
-  const dbName = process.env.MONGODB_URI || LOCAL_MONGODB_URI;
+  let uri;
+  if (process.env.MONGODB_URI) {
+    uri = process.env.MONGODB_URI;
+  } else {
+    if (env === undefined) {
+      throw new Error('Cannot resolve dbName, NODE_ENV not defined');
+    }
+    const map = { development: '_dev', test: '_test', production: '' };
+    const dbName = `o6ua6_next${map[env]}`;
+    uri = `mongodb://localhost:27017/${dbName}`;
+  }
   mongoose.Promise = Promise;
-  await mongoose.connect(dbName);
+  await mongoose.connect(uri);
   mongoose.connection.on(
     'error', console.error.bind(console, 'mongodb connection error:')
   );
-  return mongoose;
+  return { mongoose, dbName: mongoose.connection.name };
 };
 
 const disconnect = () => mongoose.disconnect();
